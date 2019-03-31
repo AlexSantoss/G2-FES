9894480c9e77cd3001ee8ae03f6b2a3910e94593
ssousa97 <samuelsousa064@gmail.com>
2019-03-31 12:15:23 -0300

    atualizações

---
+++ src/cliente/ClienteController.java
@@ -0,0 +1,24 @@
+package cliente;
+
+import javafx.fxml.FXML;
+
+public class ClienteController {
+
+
+    //cadastra novo cliente no banco de dados
+    @FXML
+    public static boolean cadastraCliente(Cliente c){
+        return true;
+    }
+
+
+    //verifica histórico do cliente, retorna false caso histórico seja inválido
+    @FXML
+    public static boolean verificaHistorico(Cliente c){
+
+    }
+
+
+
+
+}
