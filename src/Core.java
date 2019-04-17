395be868c6fd0d8139ae6d651a0fdde884763d8e
angelo-daumas <42705006+angelo-daumas@users.noreply.github.com>
2019-04-17 09:41:48 -0300

    Erro de sintaxe corrigido em Core.java
    - Estava impedindo rodar a build
    - Erro causado por commit com conflitos sendo realizado.

--- src/Core.java
+++ src/Core.java
@@ -7,11 +7,8 @@ import javafx.stage.Stage;
 public class Core extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-<<<<<<< HEAD
         Parent root = FXMLLoader.load(getClass().getResource("ui/UI.fxml"));
-=======
-        Parent root = FXMLLoader.load(getClass().getResource("cliente/Cliente.fxml"));
->>>>>>> e5d395e1e6dfcb69a27e562d78753b222ea1ae12
+        // Parent root = FXMLLoader.load(getClass().getResource("cliente/Cliente.fxml"));
         Scene scene = new Scene(root);
 
         primaryStage.setScene(scene);
