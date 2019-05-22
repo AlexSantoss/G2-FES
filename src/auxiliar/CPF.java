3ba78182034bcf10c5c0deb5292fca544f4fc34b
angelo-daumas <42705006+angelo-daumas@users.noreply.github.com>
2019-05-22 09:51:34 -0300

    Adicionar docs à clase CPF.

--- src/auxiliar/CPF.java
+++ src/auxiliar/CPF.java
@@ -5,40 +5,70 @@
  */
 package auxiliar;
 
+/**
+ * Essa classe representa um CPF na forma de string. É uma classe imutável.
+ * 
+ * @author Alex
+ *
+ */
 public class CPF{
 	private String cpf;
 
+	/**
+	 * Construtor que recebe CPF na forma de uma string. O CPF deve estar formatado na forma xxx.yyy.zzz-ww. Além disso, o CPF deve ser válido, seguindo os algoritimos que validam um CPF.
+	 * 
+	 * @param cpf CPF na forma de uma String. Se estiver malformado ou inválido, lança IllegalArgumentException.
+	 */
 	public CPF(String cpf){
 		setCPF(cpf);
 	}
 
+	/**
+	 * Seta o valor do CPF (usado apenas no construtor). Usa algoritimo para checar se o CPF é válido.
+	 * 
+	 * @param cpf
+	 */
 	private void setCPF(String cpf){
 		if(cpf.length() == 14 && cpf.charAt(3) == '.' && cpf.charAt(7) == '.' && cpf.charAt(11) == '-') {
 			this.cpf = cpf;
 
 			int soma=0;
 			for(int idx=0, aux=10; idx < 11; idx++, aux--) {
-				if(idx != 3 && idx != 7) soma += Character.getNumericValue(cpf.charAt(idx)) * aux;
-				else aux = aux+1;
+				if(idx != 3 && idx != 7) 
+					soma += Character.getNumericValue(cpf.charAt(idx)) * aux;
+				else 
+					aux = aux+1;
 			}
+			
 			int resto1 = (soma*10)%11;
-			if(resto1 == 10) resto1 = 0;
+			if(resto1 == 10)
+				resto1 = 0;
 
 			soma=0;
 			for(int idx=0, aux=11; idx < 13; idx++, aux--) {
-				if(idx != 3 && idx != 7 && idx != 11) soma += Character.getNumericValue(cpf.charAt(idx)) * aux;
-				else aux = aux+1;
+				if(idx != 3 && idx != 7 && idx != 11) 
+					soma += Character.getNumericValue(cpf.charAt(idx)) * aux;
+				else 
+					aux = aux+1;
 			}
+			
 			int resto2 = (soma*10)%11;
-			if(resto2 == 10) resto2 = 0;
+			if(resto2 == 10) 
+				resto2 = 0;
 
-			if(resto1 == Character.getNumericValue(cpf.charAt(12)) && resto2 == Character.getNumericValue(cpf.charAt(13))) this.cpf = cpf;
-			else throw new IllegalArgumentException("CPF invalido");
+			if(resto1 == Character.getNumericValue(cpf.charAt(12)) && resto2 == Character.getNumericValue(cpf.charAt(13))) 
+				this.cpf = cpf;
+			else 
+				throw new IllegalArgumentException("CPF inválido.");
 		}
 		else
-			throw new IllegalArgumentException("CPF malformado");
+			throw new IllegalArgumentException("CPF malformado.");
 	}
 	
+	/**
+	 * Retorna uma String contendo o CPF. Essa string é idêntica à string passada no construtor do CPF.
+	 * 
+	 */
 	public String toString(){
 		return this.cpf;
 	}
